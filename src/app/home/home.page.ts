d9f34f3ee37a919ded5b97e647a27efb16e01636
elingeniero3 <otakuadri13@gmail.com>
2025-12-08 15:53:50 +0000

    Home Page creation and first configuration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { addIcons } from 'ionicons';
+import { heart,settingsOutline} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  ingredients:string="";
+
+  constructor() {
+    addIcons({ heart, settingsOutline});
+  }
+
+  openFavourites() {
+    //TBD
+  }
+  openSettings() {
+    //TBD
+  }
+  onSearch() {
+    //TBD
+  }
+
+  
 }
