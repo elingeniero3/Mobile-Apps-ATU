f16506765f5271cdc7418349491f273dd872b75d
elingeniero3 <otakuadri13@gmail.com>
2025-12-09 17:33:38 +0000

    Creation of favourite,recipe-details and settings pages. Configuration of the settings radio buttons with local storage for Metric and US options

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonInput} from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { heart,settingsOutline} from 'ionicons/icons';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, FormsModule, IonInput],
 })
 export class HomePage {
   ingredients:string="";
 
-  constructor() {
+  constructor(private router: Router) {
     addIcons({ heart, settingsOutline});
   }
-
+  
   openFavourites() {
-    //TBD
+    this.router.navigate(['/favourite']);
   }
   openSettings() {
-    //TBD
+    this.router.navigate(['/settings']);
   }
   onSearch() {
     //TBD
---
+++ src/app/settings/settings.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Data } from '../services/my-data';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class SettingsPage implements OnInit {
+
+  measurement: string = 'Metric'; 
+  constructor(private mds: Data) { }
+
+  async ngOnInit() {
+    const saved = await this.mds.get('Measurement');
+    if (saved === 'Metric' || saved === 'US') {
+      this.measurement = saved;
+    }
+    else{
+      await this.mds.set('Measurement', this.measurement);
+    }
+  }
+  
+  async onMeasurementChange(value: string) {
+  if (value !== this.measurement) {
+    this.measurement = value;
+    await this.mds.set("Measurement",this.measurement);
+  }
+}
+}
