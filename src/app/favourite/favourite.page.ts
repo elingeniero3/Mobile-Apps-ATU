3d6a821c9d1dcc263de15015191b21bf753615fb
elingeniero3 <otakuadri13@gmail.com>
2025-12-11 15:18:07 +0000

    favourite page configurated to display the favourite elements

--- src/app/favourite/favourite.page.ts
+++ src/app/favourite/favourite.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { Data } from '../services/my-data';
+
 
 @Component({
   selector: 'app-favourite',
   templateUrl: './favourite.page.html',
   styleUrls: ['./favourite.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonButton]
 })
 export class FavouritePage implements OnInit {
+  favourites: any[] = [];
+  constructor(private router: Router,private mds: Data) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    const favs = await this.mds.get('FAVOURITE_KEY');
+    this.favourites = favs || [];
+    console.log('FAVOURITES:', this.favourites);
   }
 
+  async openDetails(id: number) {
+    await this.mds.set('selectedRecipeId', id);
+    this.router.navigate(['/recipe-details']);
+  }
 }
